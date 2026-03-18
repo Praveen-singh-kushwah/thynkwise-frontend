97c95ffd0196ff1beb649575ec6eec1be540569a
Praveen-singh-kushwah <praveensingh7990@gmail.com>
2026-03-18 16:46:20 +0530

    remove unnecessary images and fix service page image icon rendering issues

--- components/Main/Service/Section1/Section1.js
+++ components/Main/Service/Section1/Section1.js
@@ -1,6 +1,6 @@
 import style from "./Section1.module.css";
 import Image from "next/image";
-import { getServicesPage } from "../strapi/service";
+import { getMediaUrl } from "@/lib/media";
 
 function makeTablesResponsive(html) {
   if (!html) return html;
@@ -25,7 +25,6 @@ function makeTablesResponsive(html) {
 }
 
 export default async function Section1({ services }) {
-
   if (!services) return null;
 
   return (
@@ -33,10 +32,7 @@ export default async function Section1({ services }) {
       <div className="container">
         <div className="row gy-md-5 gy-sm-3">
           {services.map((service, index) => {
-            const iconUrl =
-              service.icon?.url
-                ? "http://localhost:1337" + service.icon.url
-                : null;
+            const iconUrl = getMediaUrl(service.icon?.url);
 
             return (
               <div key={index} className="col-lg-12 col-sm-6 p-0">
@@ -46,8 +42,8 @@ export default async function Section1({ services }) {
                       <div className={style["em-service-icon"]}>
                         <Image
                           src={iconUrl}
-                          width={40}
-                          height={40}
+                          width={service.icon?.width || 40}
+                          height={service.icon?.height || 40}
                           alt={service.title}
                         />
                       </div>
